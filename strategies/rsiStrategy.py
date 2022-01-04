9e7027761dab87ede9ce2ebb69c237959770e183
Szel Kalman <kalman.szel@bitotron.com>
2022-01-05 00:34:00 +0100

    WIP RSI strategy

--- strategies/rsiStrategy.py
+++ strategies/rsiStrategy.py
@@ -1,6 +1,10 @@
 import json
 import queue
+import talib as ta
+from typing import List
 from services.exchange.models.assetKline import AssetKline
+from services.exchange.models.timeFrame import TimeFrame
+from services.technicalAnalysis import TalibCompatible, TechnicalAnalysis
 from strategies.stategyBase import StrategyBase
 
 """
@@ -10,8 +14,8 @@ RSI_SELL = 25
 timeFrame = 15m
 
 LISTENING_SIGNAL = RSI == 25
-buySignal = (RSI < 25) AND (FIRST GREEN VOLUME) 
-sellSignal = 
+buySignal = (RSI < 25) AND (FIRST GREEN VOLUME)
+sellSignal =
     RSI == 40
     OUT on 1%?
 
@@ -20,33 +24,43 @@ sellSignal should be based on volume!
 """
 
 
-class RsiStrategy(StrategyBase):
+class RsiStrategySettings:
     def __init__(self, length, buy, sell) -> None:
-        super().__init__()
         self.RSI_LENGTH = length
         self.RSI_BUY = buy
         self.RSI_SELL = sell
-        self.queue = queue.Queue(self.RSI_LENGTH)
 
-    def update(self, data):
-        kline = self.convertToKline(data)
-        self.preprocess(kline)
 
-    def preprocess(self, kline: AssetKline):
-        if(kline.isClosed):
-            self.queue.put(kline)
-            self.checkForSignal()
-            self.queue.get()
+class RsiStrategyTechnicalAnalysis:
+    def __init__(self, data: TalibCompatible, latestKline: AssetKline) -> None:
+        # TODO
+        self.RSI14 = TechnicalAnalysis.getRSI(data.close, 14)
+        self.latestKline = latestKline
 
-    def checkForSignal(self):
-        klineHistoy = list(self.queue.queue)
-        rsi = 0
+    def __str__(self) -> str:
+        return "RSI 14: {},\nLatest kline: {}\n".format(self.RSI14, self.latestKline)
 
+
+class RsiStrategy(StrategyBase):
+    def __init__(self, settings: RsiStrategySettings) -> None:
+        super().__init__(settings.RSI_LENGTH)
+        self.settings = settings
+
+    def update(self, klines: List[AssetKline]):
+        convertedKline = TechnicalAnalysis.convertToTalibCompatible(klines)
+        processed = RsiStrategyTechnicalAnalysis(convertedKline, klines[-1])
+
+        self.checkForSignal(processed)
+
+    def checkForSignal(self, data: RsiStrategyTechnicalAnalysis):
+        print("RSI STRATEGY: {}".format(data))
+        """
         if(rsi <= self.RSI_BUY):
             self.onBuySignal()
 
         if(rsi >= self.RSI_SELL):
             self.onSellSignal()
+        """
 
     def onBuySignal(self):
         inPosition = False
