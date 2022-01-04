423d20b41667ad8273159011ca6a4777bff72ae8
Szel Kalman <kalman.szel@bitotron.com>
2022-01-05 00:33:08 +0100

    Time fields added

--- services/exchange/models/assetKline.py
+++ services/exchange/models/assetKline.py
@@ -1,11 +1,13 @@
 class AssetKline:
-    def __init__(self, open, high, low, close, volume, isClosed) -> None:
+    def __init__(self, open, high, low, close, volume, isClosed, startTime, closeTime) -> None:
         self.open = open
         self.high = high
         self.low = low
         self.close = close
         self.volume = volume
         self.isClosed = isClosed
+        self.startTime = startTime
+        self.closeTime = closeTime
 
     def __str__(self):
-        return "Open:{} High:{} Low:{} Close:{} Volume:{} isClosed:{}\n".format(self.open, self.high, self.low, self.close, self.volume, self.isClosed)
+        return "Open:{}\tHigh:{}\tLow:{}\tClose:{}\tVolume:{}\tisClosed:{}\n".format(self.open, self.high, self.low, self.close, self.volume, self.isClosed)
