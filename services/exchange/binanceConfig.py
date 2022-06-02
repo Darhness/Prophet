d1121468c0f80a40b59eba7b19cc5dd7b9713dc5
Kalman Szel <darhness@gmail.com>
2022-06-02 22:56:02 +0200

    binance config changes

--- services/exchange/binanceConfig.py
+++ services/exchange/binanceConfig.py
@@ -1,3 +1,6 @@
 BINANCE_API_URL = "https://api.binance.com/api/v3/"
 BINANCE_SOCKET_API_URL = "wss://stream.binance.com:9443/ws/"
 BINANCE_AUTH_TOKEN = ""
+
+BINANCE_API_KEY = ""
+BINANCE_SECRET_KEY = ""
