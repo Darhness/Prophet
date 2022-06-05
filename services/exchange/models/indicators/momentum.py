5202ee91ceaffef4d06f74fa58865916fc5fc2ce
Kalman Szel <darhness@gmail.com>
2022-06-05 23:05:44 +0200

    Removed unnecessary files

--- services/exchange/models/indicators/momentum.py
+++
@@ -1,9 +0,0 @@
-class Momentum:
-    """ 
-    * RSI is relative strength index
-    * CCI is commodity channel index
-    """
-
-    def __init__(self) -> None:
-        self.RSI14 = 0
-        self.CCI = 0
--- services/exchange/models/indicators/volume.py
+++
@@ -1,9 +0,0 @@
-class Volume:
-    """
-    * CHA is chaikin oscillator
-    * OBV is on-balance volume
-    """
-
-    def __init__(self) -> None:
-        self.OBV = 0
-        self.CHA = 0
